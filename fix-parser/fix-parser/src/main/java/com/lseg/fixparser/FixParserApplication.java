4f77f7bf5bc18ec54e9ee281060e12a85f7200fd
Likhith.GP <likhith.gp@legatohealth.com>
2024-02-20 23:00:12 +0530

    Fix parser

---
+++ fix-parser/fix-parser/src/main/java/com/lseg/fixparser/FixParserApplication.java
@@ -0,0 +1,13 @@
+package com.lseg.fixparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixParserApplication.class, args);
+	}
+
+}
---
+++ fix-parser/fix-parser/src/test/java/com/lseg/fixparser/FixParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lseg.fixparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
